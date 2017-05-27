bd5bc4a36339842f04c19aa3686f87849f1ec9d4
Tudor-George Motrescu <tudor.g.motrescu@gmail.com>
2017-05-27 16:38:40 +0300

    Triggers Reorder!

--- Database-Backend/sql-files/triggers.sql
+++ Database-Backend/sql-files/triggers.sql
@@ -1,6 +1,4 @@
-set serveroutput on;
-
-create or replace trigger createUser
+create or replace trigger createUserTrigger
 after insert on players
 referencing new as newRow
 for each row
@@ -27,18 +25,41 @@ begin
 	city_max_id := city_max_id + 1;
 	INSERT INTO CITY(users_id, cityID, cityPosX, cityPosY, cityPoints, StorageWood, StorageStone, StorageFood, StorageMaxCapacity)
 	VALUES(:newRow.userid, city_max_id, dbms_random.value(0, 1000), dbms_random.value(0, 1000), 500, 200, 200, 200, 1000);
-	
+
 	build_entity_id := build_entity_id + 1;
 	insert into user_buildings(users_id, building_id, cityID, building_points, entity_id, building_level)
 	values(:newRow.userid, 1, city_max_id, 200, build_entity_id,1);
-	
+
 	build_entity_id := build_entity_id + 1;
 	insert into user_buildings(users_id, building_id, cityID, building_points, entity_id, building_level)
 	values(:newRow.userid, 2, city_max_id, 250, build_entity_id,1);
-	
+
 	build_entity_id := build_entity_id + 1;
 	insert into user_buildings(users_id, building_id, cityID, building_points, entity_id, building_level)
 	values(:newRow.userid, 3, city_max_id, 300, build_entity_id,1);
 end;
 
-INSERT INTO PLAYERS(userid, username, email, passwd) VALUES(usr_procedure_id, nm_usr, email_usr, pass_usr);
+
+create or replace trigger deleteUserTrigger
+after delete on players
+referencing old as oldRow
+for each row
+declare
+ v_user_id int;
+begin
+  select users_id into v_user_id from city where users_id = :oldRow.userid;
+  execute immediate 'delete from user_buildings where users_id = '||v_user_id;
+  execute immediate 'delete from city where users_id = '||v_user_id;
+end;
+
+
+create or replace trigger deleteCityTrigger
+after delete on city
+referencing old as oldRow
+for each row
+declare
+ v_city_id int;
+begin
+  select cityid into v_city_id from user_buildings where cityid = :oldRow.cityid;
+  execute immediate 'delete from user_buildings where cityid = '||v_city_id;
+end;
