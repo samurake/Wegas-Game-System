3bcc911b6c7749e6797da9411d7c7b7453d20a7d
Tudor-George Motrescu <tudor.g.motrescu@gmail.com>
2017-05-27 12:16:37 +0300

    Folder reorder!

--- Database Backend/exceptionhandler.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE PACKAGE exceptionhandler
-AS
-  insert_null_into_notnull EXCEPTION;
-  PRAGMA EXCEPTION_INIT(insert_null_into_notnull, -1400);
-
-  update_null_to_notnull EXCEPTION;
-  PRAGMA EXCEPTION_INIT(update_null_to_notnull, -1407);
-END exceptionhandler;
-
