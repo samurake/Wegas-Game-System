46ccc727993ffdedd4133672d1db95a8be812e7b
RazvanIulianLupu <razvaniulianlupu@yahoo.ro>
2017-06-08 21:01:38 +0300

    all libs are added now + woodtile

---
+++ Backend/js/MedievalCity/Tile/MedievalCity.WoodTile.js
@@ -0,0 +1,26 @@
+MedievalCity.WoodTile = function () {
+
+    MedievalCity.Tile.call( this );
+    this.meshObject = 'log';
+    this.meshTexture = 'log';
+
+}
+
+MedievalCity.WoodTile.prototype = Object.create( MedievalCity.Tile.prototype );
+
+MedievalCity.WoodTile.prototype.create = function () {
+
+    MedievalCity.Tile.prototype.create.call(this);
+
+    this.object.scale.x = 0.07;
+    this.object.scale.y = 0.04;
+    this.object.scale.z = 0.04;
+    return this.object;
+}
+
+MedievalCity.WoodTile.prototype.select = function() {
+    return false;
+}
+MedievalCity.WoodTile.prototype.deselect = function() {
+    return false;
+}
